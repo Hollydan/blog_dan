9d5e8c0674e90d4292fdcf7ec2e75ed105b4a181
Hollydan <2642956839@qq.com>
2017-11-22 12:07:53 +0800

    User can upload avatar

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -9,9 +9,64 @@
 namespace App\Handlers;
 
 
+use function foo\func;
+use Intervention\Image\Image;
+
 class ImageUploadHandler
 {
     // 只允许以下后缀名的图片文件上传
     protected $allow_ext = ['jpg', 'jpeg', 'png', 'gif', 'bmp'];
+
+    public function save($file, $folder, $file_prefix, $max_width = false)
+    {
+        //存储的文件夹规则
+        $folder_name = 'uploads/images/' . $folder . '/' . date('Ym', time()) . '/' . date('d', time()) . '/';
+
+        //图片存储的物理路径
+        $upload_path = public_path() . '/' . $folder_name;
+
+        //获取文件后缀，因图片从剪切板里粘贴时后缀名为空，所以此处确保后缀名一直存在
+        $extension = strtolower($file->getClientOriginalExtension()) ? : 'png';
+
+        //拼接文件名，前缀可以使相关数据模型的 ID
+        $filename = $file_prefix . '_' . time() . '_' . str_random(10) . '.' .$extension;
+
+        //如果上传的不是图片将终止操作
+        if (! in_array($extension, $this->allow_ext)) {
+            return false;
+        }
+
+        //将图片移动到目标存储路径
+        $file->move($upload_path, $filename);
+
+        //如果限制了图片宽度，进行裁剪
+        if ($max_width && $extension != 'gif') {
+
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
+        return [
+            'path' => config('app.url') . '/' . $folder_name . '/' . $filename,
+        ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        //先实例化，参数为文件的磁盘物理路径
+        $image = Image::make($file_path);
+
+        //进行大小调整的操作
+        $image->resize($max_width, null, function ($constraint) {
+
+            //设定宽度为$max_width，高度等比例缩放
+            $constraint->aspectRatio();
+
+            //防止截图时图片尺寸变大
+            $constraint->upsize();
+        });
+
+        //保存修改的图片
+        $image->save();
+    }
 
 }
--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,11 +28,21 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=200,min_height=200',
         ];
     }
 
-    public function message()
+    public function messages()
     {
-
+        return [
+            'name.required' => '用户名不能为空',
+            'name.between' => '用户名必须介于 3 - 25 个字符之间',
+            'name.regex' => '用户名只支持字母、数字、下划线、中划线',
+            'name.unique' => '用户名已被占用，请重新填写',
+            'email.required' => '邮箱不能为空',
+            'email.email' => '邮箱格式不正确，请重新填写',
+            'introduction.max' => '简介最多80个字符',
+            'avatar.dimensions' => '图片清晰度不够，宽和高需要200px以上',
+        ];
     }
 }
